e928f64835d6e51b680e85964d160746d79544fd
Sandesh Singh <sandesh1014@gmail.com>
2020-04-09 20:40:09 +0530

    timel

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <v-app>
+      <main>
+        <Header />
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-col dark cols="12" sm="12" md="12" class="my-4">
+    <v-card color="grey lighten-4" flat height="64px">
+      <v-toolbar class="primary">
+        <v-avatar>
+          <v-img :src="require('@/assets/devlup.jpeg')" />
+        </v-avatar>
+        <v-spacer></v-spacer>
+        <v-btn text dark>
+          Home
+        </v-btn>
+        <v-btn text dark>
+          Projects
+        </v-btn>
+        <v-btn text dark>
+          About
+        </v-btn>
+        <v-btn text dark>
+          Timeline
+        </v-btn>
+        <v-btn text dark>
+          Blog
+        </v-btn>
+        <v-btn text dark>
+          Contact
+        </v-btn>
+      </v-toolbar>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
